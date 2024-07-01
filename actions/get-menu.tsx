c7fc1a4ee3c851e9baf835556da921eb48f35a06
lucas <lucas.chorolqui@gmail.com>
2024-06-30 21:16:00 -0300

    changed fetch method by axios

--- actions/get-menu.tsx
+++ actions/get-menu.tsx
@@ -1,13 +1,9 @@
+import axios from "axios";
 import { Drink } from "../types";
 
 const URL = "http://localhost:3001/api/drinks";
 
 export default async function getAllDrinks(): Promise<Drink[]> {
-  const res = await fetch(URL, {
-    headers: {
-      "Cache-Control": "must-revalidate, no-cache, no-store, max-age=5",
-    },
-  });
-  const data = await res.json();
-  return data;
+  const res = await axios.get(URL);
+  return res.data;
 }
