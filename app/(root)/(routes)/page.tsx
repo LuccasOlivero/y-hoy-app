8276d89a41d1a5a75a9e8f3beb5b59d494852151
lucas <lucas.chorolqui@gmail.com>
2024-07-15 20:59:17 -0300

    fix - hydration error managment

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Main from "./components/main";
+import { useEffect, useState } from "react";
 
 export default function SetupHome() {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-    <div className="container mx-auto">
-      <Header />
-      <Main />
+    <>
+      <div className="container mx-auto">
+        <Header />
+        <Main />
+      </div>
+
       <Footer />
-    </div>
+    </>
   );
 }
