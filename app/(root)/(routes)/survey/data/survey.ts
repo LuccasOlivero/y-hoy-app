d3493c0cee60cf15a325d85f1058758f0cc8fd49
lucas <lucas.chorolqui@gmail.com>
2024-07-22 13:19:31 -0300

    test questions added

--- app/(root)/(routes)/survey/data/survey.ts
+++ app/(root)/(routes)/survey/data/survey.ts
@@ -1,5 +1,3 @@
-import { text } from "stream/consumers";
-
 export const questions = [
   {
     id: 1,
@@ -7,6 +5,8 @@ export const questions = [
     options: [
       { id: 1, response: "Dulces" },
       { id: 2, response: "Amargas" },
+      { id: 3, response: "Amargas" },
+      { id: 4, response: "Amargas" },
     ],
     text: [
       {
@@ -19,8 +19,42 @@ export const questions = [
     id: 2,
     title: "¿Te gustan las bebidas con alcohol?",
     options: [
-      { id: 3, response: "Sí" },
-      { id: 4, response: "No" },
+      { id: 5, response: "Sí" },
+      { id: 6, response: "No" },
+      { id: 7, response: "Amargas" },
+      { id: 8, response: "Amargas" },
+    ],
+    text: [
+      {
+        en: "For hot days, we recommend beer and refreshing drinks. For cold, we recommend robust and concentrated drinks. For dinner, we recommend fresh and sweet drinks.",
+        es: "Para días calurosos te recomendamos cervezas de cuerpo liviano y refrescantes. Para el frío, cervezas más robustas y contundentes. Para la cena, te recomendamos cervezas de fuentes frescas y dulces.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "¿Te gustan las bebidas con alcohol?",
+    options: [
+      { id: 9, response: "Sí" },
+      { id: 10, response: "No" },
+      { id: 11, response: "Amargas" },
+      { id: 12, response: "Amargas" },
+    ],
+    text: [
+      {
+        en: "For hot days, we recommend beer and refreshing drinks. For cold, we recommend robust and concentrated drinks. For dinner, we recommend fresh and sweet drinks.",
+        es: "Para días calurosos te recomendamos cervezas de cuerpo liviano y refrescantes. Para el frío, cervezas más robustas y contundentes. Para la cena, te recomendamos cervezas de fuentes frescas y dulces.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "¿Te gustan las bebidas con alcohol?",
+    options: [
+      { id: 13, response: "Sí" },
+      { id: 14, response: "No" },
+      { id: 15, response: "Amargas" },
+      { id: 16, response: "Amargas" },
     ],
     text: [
       {
